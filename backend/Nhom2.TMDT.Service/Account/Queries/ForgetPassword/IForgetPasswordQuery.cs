e2339f17b4beb363221dbb08c6b8e8436639e619
minhtan143 <minhtan143@users.noreply.github.com>
2020-07-10 19:32:07 +0700

    create forgetpass, register, sendmail

---
+++ backend/Nhom2.TMDT.Service/Account/Queries/ForgetPassword/IForgetPasswordQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Nhom2.TMDT.Service.Account.Queries.ForgetPassword
+{
+    public interface IForgetPasswordQuery
+    {
+        Task<bool> ExecutedAsync(string email);
+    }
+}
--- backend/Nhom2.TMDT.Service/Account/Queries/Register/RegisterQuery.cs
+++ backend/Nhom2.TMDT.Service/Account/Queries/Register/RegisterQuery.cs
@@ -1,24 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Nhom2.TMDT.Common.Encryption;
 using Nhom2.TMDT.Common.Verification;
 using Nhom2.TMDT.Data.Services;
 using Nhom2.TMDT.Service.Account.ViewModels;
 using Nhom2.TMDT.Service.Mail.SendMail;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Nhom2.TMDT.Service.Account.Queries.Register
 {
-    public class RegisterQuery
+    public class RegisterQuery : IRegisterQuery
     {
         private readonly ApplicationContext db;
         private readonly ISendMail sendMail;
         private Encryption encryption;
         private Verification verification;
+        private readonly IHostingEnvironment environment;
 
-        public RegisterQuery(ApplicationContext db, ISendMail sendMail)
+        public RegisterQuery(ApplicationContext db, ISendMail sendMail, IHostingEnvironment environment)
         {
             this.db = db;
             this.sendMail = sendMail;
+            this.environment = environment;
             encryption = new Encryption();
             verification = new Verification();
         }
@@ -32,7 +36,12 @@ namespace Nhom2.TMDT.Service.Account.Queries.Register
 
                 string verificationCode = verification.Random();
 
-                if (await sendMail.ExecutedAsync(registerViewModel.Email, "", ""))
+                string body = File.ReadAllText("./Templates/MailConfirmTemplate.html");
+
+                body = body.Replace("@Name", registerViewModel.FullName);
+                body = body.Replace("@Link", "http://localhost:4200/Activation?username=" + registerViewModel.Email + "&code=" + verificationCode);
+
+                if (await sendMail.ExecutedAsync(registerViewModel.Email, "Account activation", body))
                 {
                     string passwordEncrypt = encryption.EncryptionMD5(registerViewModel.Password);
 
