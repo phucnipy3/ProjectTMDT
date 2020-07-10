12fd598a7561ba9c7219716ec0cb2545a13b1e28
minhtan143 <minhtan143@users.noreply.github.com>
2020-07-10 19:59:30 +0700

    add edit, delete comment

---
+++ backend/Nhom2.TMDT.Service/Product/Commands/DeleteCommand/IDeleteCommentCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Nhom2.TMDT.Service.Product.Commands.DeleteCommand
+{
+    public interface IDeleteCommentCommand
+    {
+        Task<bool> ExecutedAsync(int userId, int commentId);
+    }
+}
